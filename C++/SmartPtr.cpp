22fc4b47edea92b74c11793e052352ac05bf311a
Piyush Jaiswal <thejaiswal1@gmail.com>
2024-11-25 15:45:25 +0530

    Added C++ concepts folder

---
+++ C++/SmartPtr.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+class SmartPrt{
+    private:
+        int* ptr;
+    
+    public:
+        SmartPrt(){
+            ptr = new int;
+            cout << "Constructor Called" << endl;
+        }
+
+        ~SmartPrt(){
+            delete ptr;
+            cout << "Destructor Called" << endl;
+        }
+
+        int* usePtr(){
+            return ptr;
+        }
+};
+
+int main(){
+    {
+        SmartPrt intPtr;
+        *intPtr.usePtr() = 20;
+        cout << "value :" << *intPtr.usePtr() << endl;
+    }
+}
